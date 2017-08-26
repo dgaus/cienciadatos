4c16e29d4b9334ffcfbec734c60a60bb76e426f5
Nambroa <nambroa@eryxsoluciones.com.ar>
2017-08-26 14:42:17 -0300

    Cambiado el test para la primer pregunta del TP, que estaba mal. Agregado grafico scatter. Borrado de la clase que iba a hacer los graficos, me di cuenta de que era muy dificil hacerla dinamica asi que mejor usar las librerias de una me parece.

--- tp1/athlete_graphics.py
+++
@@ -1,2 +0,0 @@
-import matplotlib.pyplot as plt  # Graficos
-import seaborn as sns  # Graficos Lindos
--- tp1/main.py
+++ tp1/main.py
@@ -1,4 +1,6 @@
+# coding=utf-8
 import numpy as np
+import matplotlib.pyplot as plt  # Graficos
 from athlete_tester import AthleteTester
 from recorded_athlete_times import RecordedAthleteTimes
 
@@ -10,12 +12,26 @@ times_for_sunny_days = recorded_athlete_times.times_for_sunny_days()
 
 """
 
-Test if athletes are slower on rainy days than on sunny days. We check whether the average expected value
-differs significantly between those 2 samples. Null hyphotesis: average times are almost equal or identical.
-Since the samples have different variances, we use welcher't test.
+Son los atletas son más lentos en dı́as de lluvia que en dı́as soleados? Usando un related t-test (elegimos ese al tener
+2 muestras de la misma poblacion), tratamos de rechazar la hipotesis nula de que ambas muestras tienen identicos
+valores promedio (esperados).
 
 """
-assert(np.var(times_for_rainy_days) != np.var(times_for_sunny_days))
-indep_test = tester.make_welch_t_test_on(times_for_rainy_days, times_for_sunny_days)
-print(indep_test)
-# Since the p-value is very small (less than 1%), we can reject the null hypothesis.
+rel_test = tester.make_related_samples_t_test_on(times_for_rainy_days, times_for_sunny_days)
+print("t-statistic: %s, p-value: %s" % (rel_test[0], rel_test[1]))
+# Como el p-valor es muy chico (menor a 1%), podemos rechazar la hipotesis nula.
+# Como el t-estadistico es positivo y grande, sabemos que la media para los tiempos medidos en dias lluviosos es
+# mayor a la media de los tiempos medidos en dias soleados.
+# Podemos entonces concluir que los atletas son mas lentos en dias lluviosos que en dias soleados.
+
+# Scatter plot of the data.
+colors = ["red", "blue"]
+groups = [u"Días Lluviosos", u"Días Soleados"]
+x = np.linspace(0, 12, 12)
+figure = plt.figure()
+subplot = figure.add_subplot(1, 1, 1, facecolor="1.0")
+subplot.scatter(x=x, y=times_for_rainy_days, c=colors[0], label=groups[0])
+subplot.scatter(x=x, y=times_for_sunny_days, c=colors[1], label=groups[1])
+plt.title(u'Distribución de Tiempos Medidos en Días Lluviosos vs Soleados')
+plt.legend(loc=2)
+plt.show()
